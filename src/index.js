490066714b4d976c8417c4dd4ad7ace4ab92a6de
DESKTOP-OOM0DRK\Kimchi_Highlander <hippiengypsy02@gmail.com>
2022-06-15 18:34:18 +0100

    [REFACTOR] refactor the counter

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ const plus = document.getElementById("plus");
 const minus = document.getElementById("minus");
 const number = document.getElementById("number");
 
+const PLUS = "PLUS";
+const MINUS = "MINUS";
+
 const countModifier = (count = 0, action) => {
-  // console.log(count, action);
-  if (action.type === "PLUS") {
-    return count + 1;
-  } else if (action.type ==="MINUS") {
-    return count - 1;
-  } else {
-    return count;
+  switch (action.type) {
+    case PLUS:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 
